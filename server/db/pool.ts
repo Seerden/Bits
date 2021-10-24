f6d2ec03194c02e24a996fb8627068a20f2ef1fc
Christiaan Seerden <christiaanseerden@live.nl>
2021-10-24 17:19:10 +0200

    Install dotenv, store database pool initialization in separate file

---
+++ server/db/pool.ts
@@ -0,0 +1,13 @@
+import { Client, Pool } from 'pg';
+import { config } from 'dotenv';
+
+const { PG_HOST, PG_USER, PG_DATABASE } = process.env;
+
+export default new Pool({
+    host: PG_HOST,
+    user: PG_USER,
+    database: PG_DATABASE,
+    max: 10,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000
+});
