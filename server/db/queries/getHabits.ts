79fad7a729578f98fc4fb20c6d4298fc1f4841e5
Christiaan Seerden <christiaanseerden@live.nl>
2021-10-30 14:08:06 +0200

    Add GET /habits/u/:username route

--- server/db/queries/getHabits.ts
+++ server/db/queries/getHabits.ts
@@ -1,10 +1,28 @@
 import { makePooledQuery } from "../dbQuery";
 
-export async function getHabits(options) {
+export async function getHabits(options?: any) {
     const { rows } = await makePooledQuery({
         name: 'get all habits',
         text: 'select * from habits',
     });
 
     return rows;
+}
+
+export async function getHabitsByUser(username: string) {
+    try {
+        const { rows } = await makePooledQuery({
+            name: 'getHabitsByUser',
+            text: `
+                select h.* from habits h
+                left join users u
+                on h.user_id = u.user_id
+                and u.username = $1
+            `,
+            values: [username]
+        });
+        return rows;
+    } catch (error) {
+        throw new Error(error);
+    }
 }
--- server/routers/dbRouter.ts
+++ server/routers/dbRouter.ts
@@ -1,5 +1,6 @@
 import { NewHabit } from '@shared/types/Habit';
 import express from 'express';
+import { getHabits, getHabitsByUser } from '../db/queries/getHabits';
 import { getUsers } from '../db/queries/getUsers';
 import { insertHabit } from '../db/queries/insertHabit';
 import { insertUser } from '../db/queries/insertUser';
@@ -32,6 +33,24 @@ dbRouter.post('/user', async (req, res) => {
     }
 })
 
+dbRouter.get('/habits', async (req, res) => {
+    try {
+        const habits = await getHabits();
+        res.send(habits);
+    } catch (error) {
+        res.status(401).send('Error fetching habits from database')        
+    }
+});
+
+dbRouter.get('/habits/u/:username', async (req, res) => {
+    try {
+        const habits = await getHabitsByUser(req.params.username);
+        res.send(habits)
+    } catch (error) {
+        res.status(401).send('Error fetching habits from database')        
+    }
+})
+
 dbRouter.post('/habit', async (req, res) => {
     const newHabit: NewHabit = req.body;
 
@@ -40,7 +59,7 @@ dbRouter.post('/habit', async (req, res) => {
         res.send(insertedHabit);
     } catch (error) {
         console.error(error.stack)
-        res.status(400).send({ 
+        res.status(401).send({ 
             message: 'Error inserting habit into database',
             error
         })
