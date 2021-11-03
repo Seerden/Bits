311b81314186d57b72afa47ea6c8b29129bf470f
Christiaan Seerden <christiaanseerden@live.nl>
2021-11-03 14:59:49 +0100

    (client) Fix creation of habitEntries to use correct keys. Allow asTimes to take array of stringified dates

--- client/src/components/CompactHabit/CompactHabit.tsx
+++ client/src/components/CompactHabit/CompactHabit.tsx
@@ -3,6 +3,7 @@ import { Completion } from "../../../../shared/types/Completion";
 import './CompactHabit.scss';
 import CompletionInstance from "components/HabitInstance/CompletionInstance";
 import { useEffect } from "react";
+import { asDates, asTimes } from "helpers/time/asDates";
 
 type CompactHabitProps = {
     habitData: Habit,
@@ -21,69 +22,43 @@ const CompactHabit = ({ habitData, completionData, partitions }: CompactHabitPro
     } = habitData;
 
     const partitionsAsTimes = partitions.map(partition => partition.map(date => date.getTime()));
-
     const entriesPerDay = completionTimescale === 'day' ? completionFrequency : 1;
 
-    /*
-        Create a list of completionEntries
-        - each entry (= completionEntry) in the list contains entries for a single datePartition
-        - each completionEntry contains at least one HabitInstance, depending on the habit's completionFrequency
-        - how do we handle cases like the following: 
-            timestep = 'day', habit.completionFrequency = 1, habit.completionTimescale = 'week'
-        - put existing completionEntries (from completionData, matching a date in a given partition) in the correct spot
-    */
+    // @todo: turn this into a function
+    const habitEntries = partitionsAsTimes.map(partition =>
+        partition.map(timestamp => {
+            const foundEntry = completionData.filter(d =>
+                new Date(d.habitEntryDate).getTime() === timestamp
+            );
 
-    /**
-     *  For every date in each partition, create the appropriate number of CompletionInstances,
-     *      allocating any completion entries that already exist in the database (i.e. each entry in completionData)
-     *      to its respective CompletionInstance
-     */
-    const habitEntries = partitions.map((partition, partitionIndex) => {
-        const timestamps = partitionsAsTimes[partitionIndex];
+            return [...Array(entriesPerDay).keys()].map(entryIndex => {
+                const foundEntryAtIndex = foundEntry.filter(entry => entry.entryIndex === entryIndex)[0];
+                const [completed, rangeValue] = foundEntryAtIndex
+                    ? [foundEntryAtIndex.completed, foundEntryAtIndex.rangeValue]
+                    : [false, 0]
 
-        return partition.map(
-            date => {
-                return [...Array(entriesPerDay).keys()].map(
-                    entryIndex => {
-                        const existingEntry = completionData.filter(completionEntry => {
-                            const entryTime = new Date(completionEntry.habitEntryDate).getTime();
-                            return timestamps.includes(entryTime) && completionEntry.entryIndex === entryIndex;
-                        }
-                        )[0];
-
-                        const [completed, rangeValue] = existingEntry
-                            ? [existingEntry.completed, existingEntry.rangeValue]
-                            : [false, 0]
-
-                        return <CompletionInstance
-                            key={`${partitionIndex}-${entryIndex}`}
-                            habitId={habitData.habitId}
-                            habitEntryDate={date}
-                            {...{ completionInterval, entryIndex, completionType, completed, rangeValue }}
-                        />
-                    })
-            }
-        )
-    });
-
-    console.log(habitEntries);
+                return <CompletionInstance
+                    key={`${timestamp}-${entryIndex}`}
+                    habitEntryDate={new Date(timestamp)}
+                    {...{
+                        completed,
+                        rangeValue,
+                        completionType,
+                        completionFrequency,
+                        completionInterval,
+                        completionTimescale
+                    }}
+                />
+            })
+        })
+    )
 
     return (
         <li className={`${base}`}>
             <span>
                 {habitData.habitName}
             </span>
-
-            {
-                habitEntries.map((entry, index) =>
-                    <ul
-                        key={index}
-                        className={`${base}__entry`}
-                    >
-                        {entry}
-                    </ul>
-                )
-            }
+            {habitEntries}
         </li>
     )
 }
--- client/src/helpers/time/asDates.ts
+++ client/src/helpers/time/asDates.ts
@@ -12,10 +12,13 @@ export function asDates(dates: Dayjs[]) {
 /**
  * Convert array `dates` from `Dayjs[] | Date[]` to array of timestamps (i.e. an array typed like the result of new Date().getTime())
  */
-export function asTimes(dates: Date[] | Dayjs[]) {
+export function asTimes(dates: string[] | Date[] | Dayjs[]) {
     const toTimes = (dates: Date[]) => dates.map(date => date.getTime())
 
-    if (dates.length > 0 && 'getTime' in dates[0]) {  // dates is Date[]
+    if (typeof dates[0] === 'string') { // dates is String[]
+        return toTimes(dates.map(date => new Date(date)));
+    // @ts-ignore -- we know for a fact that dates is not a string[], so can ignore primitive warning
+    } else if (dates.length > 0 && 'getTime' in dates[0]) {  // dates is Date[]
         return toTimes(dates as Date[]);
     } else {  // dates is Dayjs[]
         return toTimes(asDates(dates as Dayjs[]));
