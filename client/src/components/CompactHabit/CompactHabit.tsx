eb0fcaa2b5dc31630fd6a97eaf41fb7ea5a9755d
Christiaan Seerden <christiaanseerden@live.nl>
2021-11-04 00:16:55 +0100

    (client) Slightly refactor CompactHabit.tsx to improve readability

--- client/src/components/CompactHabit/CompactHabit.tsx
+++ client/src/components/CompactHabit/CompactHabit.tsx
@@ -2,6 +2,7 @@ import { Habit } from "../../../../shared/types/Habit";
 import { Completion } from "../../../../shared/types/Completion";
 import './CompactHabit.scss';
 import HabitEntry from "components/HabitEntry/HabitEntry";
+import { asTimes } from "helpers/time/asDates";
 
 type CompactHabitProps = {
     habitData: Habit,
@@ -19,33 +20,28 @@ const CompactHabit = ({ habitData, completionData, partitions }: CompactHabitPro
         completionInterval
     } = habitData;
 
-    const partitionsAsTimes = partitions.map(partition => partition.map(date => date.getTime()));
+    const partitionsAsTimes = partitions.map(asTimes);
     const entriesPerDay = completionTimescale === 'day' ? completionFrequency : 1;
 
     // @todo: turn this into a function
     const habitEntries = partitionsAsTimes.map(partition =>
         partition.map(timestamp => {
-            const foundEntry = completionData.filter(d =>
+            const existingEntry = completionData.filter(d =>
                 new Date(d.habitEntryDate).getTime() === timestamp
             );
 
             return [...Array(entriesPerDay).keys()].map(entryIndex => {
-                const foundEntryAtIndex = foundEntry.filter(entry => entry.entryIndex === entryIndex)[0];
-                const [completed, rangeValue] = foundEntryAtIndex
-                    ? [foundEntryAtIndex.completed, foundEntryAtIndex.rangeValue]
-                    : [false, 0];
+                const existingEntryAtIndex = existingEntry.filter(entry => entry.entryIndex === entryIndex)[0];
+                const completed = existingEntryAtIndex?.completed || false;
+                const rangeValue = existingEntryAtIndex?.rangeValue || 0;
 
                 return {
-                    // @todo: modify key to include habit-specific property,
-                    // so that entries for different habits don't end up with same key accidentally
                     _key: `${timestamp}-${entryIndex}`,
                     habitEntryDate: new Date(timestamp),
                     completed,
                     rangeValue,
                     completionType,
-                    completionFrequency,
-                    completionInterval,
-                    completionTimescale
+                    completionInterval
                 }
             })
         })
