cc9db7863d7575b40f63ab798d67fe0f2c3138cf
Christiaan Seerden <christiaanseerden@live.nl>
2021-11-05 12:19:22 +0100

    (client) Create putHabit mutation

--- client/src/helpers/api/mutateHabits.ts
+++ client/src/helpers/api/mutateHabits.ts
@@ -1,12 +1,28 @@
 import axios from 'axios';
 import { useMutation } from 'react-query';
-import { NewHabit } from "../../../../shared/types/Habit";
+import { Habit, NewHabit } from "../../../../shared/types/Habit";
 
+// POST HABIT
 async function postNewHabit(newHabit: NewHabit) {
     return await axios.post('/api/db/habit', newHabit);
-}
+};
 
 export function usePostNewHabit() {
     const response = useMutation('postNewHabit', postNewHabit);
     return response;
-}
+};
+
+// PUT HABIT
+type PutHabit = {
+    field: keyof Habit,
+    habitToUpdate: Partial<Habit>
+};
+
+async function putHabit({ field, habitToUpdate }: PutHabit) {
+    return await axios.put('/api/db/habit', { field, habitToUpdate });
+};
+
+export function usePutHabit() {
+    const response = useMutation('putHabit', putHabit);
+    return response;
+};
