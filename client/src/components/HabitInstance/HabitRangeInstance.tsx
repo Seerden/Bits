85434f529b2ceb45afc9edbcb8eb819447e28b3d
Christiaan Seerden <christiaanseerden@live.nl>
2021-11-05 15:32:28 +0100

    HabitRangeInstance: persist changes to database, split events into change and blur to prevent unnecessary API calls

--- client/src/components/HabitInstance/HabitRangeInstance.tsx
+++ client/src/components/HabitInstance/HabitRangeInstance.tsx
@@ -1,14 +1,30 @@
 import './HabitRangeInstance.scss';
 import { useState } from 'react';
 import { CompletionInstanceProps } from 'types/CompletionInstance';
+import { useMutateCompletion } from 'helpers/api/mutateCompletion';
 
-const HabitRangeInstance = ({ rangeValue, completionInterval }: CompletionInstanceProps) => {
+const HabitRangeInstance = ({ habitId, rangeValue, completionInterval, habitEntryDate, entryIndex }: CompletionInstanceProps) => {
     const base = "HabitRangeInstance";
     const [sliderValue, setSliderValue] = useState<number>(rangeValue);
+    const { mutate } = useMutateCompletion();
 
     const progress = sliderValue >= completionInterval ? 'completed' : sliderValue >= completionInterval/2 ? 'underway' : 'incomplete'
 
-    function handleInputChangeOrBlur(e) {
+    function handleInputBlur (e) {
+        const { value } = e.target;
+        const newValue = value > 0 ? value : 0;
+
+        setSliderValue(+newValue);
+
+        mutate({
+            habitId,
+            rangeValue: +newValue,
+            habitEntryDate,
+            entryIndex
+        });
+    }
+
+    function handleInputChange(e) {
         const { value } = e.target;
         const newValue = value > 0 ? value : 0;
 
@@ -21,16 +37,16 @@ const HabitRangeInstance = ({ rangeValue, completionInterval }: CompletionInstan
                 className={`${base}__slider ${progress}`}
                 type="range" 
                 value={sliderValue}    
-                onChange={handleInputChangeOrBlur}
-                onBlur={handleInputChangeOrBlur}
+                onChange={handleInputChange}
+                onBlur={handleInputBlur}
                 max={completionInterval}
             />
             <input 
                 className={`${base}__slider--input`}
                 type="number" 
                 value={sliderValue}
-                onChange={handleInputChangeOrBlur}
-                onBlur={handleInputChangeOrBlur}
+                onChange={handleInputChange}
+                onBlur={handleInputBlur}
             />
         </div>
     )
