1b6569c3eb7f562802643ac55b4f6512ce4e3462
Christiaan Seerden <christiaanseerden@live.nl>
2021-11-06 19:32:20 +0100

    Only render relevant link buttons in Header. Add specific style to active link

--- client/src/components/Layout/Header.tsx
+++ client/src/components/Layout/Header.tsx
@@ -1,22 +1,8 @@
-import { useAuth } from "hooks/useAuth";
-import { Link } from "react-router-dom";
 import sc from './header.module.scss';
-
-const views = ['register', 'habits']
-const headerButtons = views.map((view: string) => 
-    <Link 
-        to={`/${view}`}
-        className={sc.Buttons__button}
-    >
-        {view[0].toUpperCase()+view.slice(1)}
-    </Link>
-)
+import { useHeaderLinks } from "./useHeader";
 
 const Header = (props) => {
-    const base = "Header";
-    const { currentUser, login, logout } = useAuth();
-
-    console.log(sc);
+    const links = useHeaderLinks();
 
     return (
         <div className={sc.Header}>
@@ -25,17 +11,7 @@ const Header = (props) => {
             </span>
 
             <div className={sc.Buttons}>
-                {currentUser &&
-                    <div className={sc.Header__text}>
-                        <span>Current user: {currentUser && currentUser.username}</span>
-                        <input
-                            type="button"
-                            value="log out"
-                            onClick={logout}
-                        />
-                    </div>
-                }
-                {headerButtons}
+                {links}
             </div>
         </div>
     )
---
+++ client/src/components/Layout/useHeader.tsx
@@ -0,0 +1,40 @@
+import { useAuth } from "hooks/useAuth";
+import { useCallback, useMemo } from "react";
+import { useLocation } from "react-router";
+import { NavLink } from "react-router-dom";
+import sc from './header.module.scss';
+
+export const useHeaderLinks = () => {
+    const { currentUser, logout } = useAuth();
+    const location = useLocation();
+
+    const isActive = useCallback((to) => {
+        return location.pathname.slice(1) === to
+    }, [location])
+
+    const links = useMemo(() => {
+        const links: string[] = currentUser?.username ? ['habits', 'logout'] : ['login', 'register'];
+
+        return links.map(view =>
+            <NavLink 
+                end
+                className={`${sc.Buttons__button}`}
+                style={{
+                    backgroundColor: isActive(view) ? 'deepskyblue': '',
+                    color: isActive(view) ? '#111' : 'azure'
+                }}
+                to={view === 'logout' ? 'habits' : view}
+                onClick={view === 'logout' ? () => logout(): () => { return }}
+            >
+                {view[0].toUpperCase() + view.slice(1)}
+            </NavLink>
+        );
+    }, [currentUser]);
+
+    console.log(location.pathname);
+
+    
+
+    return links;
+    
+};
