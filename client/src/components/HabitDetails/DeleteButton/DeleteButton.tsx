f26194e9f3109021611967a78e699f9bdd2997f3
Christiaan Seerden <christiaanseerden@live.nl>
2021-11-21 19:53:22 +0100

    (HabitDetails) extract DeleteButton to its own file. Do a bit of styling, and add comments and section guides

---
+++ client/src/components/HabitDetails/DeleteButton/DeleteButton.tsx
@@ -0,0 +1,73 @@
+import { useDeleteHabit } from "helpers/api/mutateHabits";
+import { useToggle } from "hooks/useToggle";
+import cs from "../HabitDetails.module.scss";
+
+/**
+ * Button element that, on confirmation, requests the deletion of the entire habit
+ * (and associated completion entries) from the API. There is an intermediate state
+ * in the form of the ConfirmDelete component -- a user must confirm that they wish to
+ * delete the habit to prevent accidental deletions.
+ */
+function DeleteButton({ habitId }: { habitId: string }) {
+	const { mutate, data } = useDeleteHabit(habitId);
+	const [confirming, toggleConfirming] = useToggle({ initial: false });
+
+	// useEffect(() => {
+	// 	/*  to ensure the just-deleted habit is removed from view
+	//      on successful deletion, either
+	//          1. update habitsState
+	//          2. refetch habits
+	//
+	//     */
+	// }, [data]);
+
+	function handleConfirm() {
+		mutate(habitId);
+	}
+
+	return !confirming ? (
+		<input
+			className={cs.Delete}
+			type="button"
+			value="Delete habit"
+			onClick={(e) => {
+				e.stopPropagation();
+				toggleConfirming();
+			}}
+		/>
+	) : (
+		<ConfirmDelete {...{ handleConfirm, toggleConfirming }} />
+	);
+}
+
+/**
+ * These two buttons are shown when a user clicks on DeleteButton. 
+ * - DELETE:    user wants to delete habit
+ * - KEEP:      user wants to keep habit.
+ */
+function ConfirmDelete({ handleConfirm, toggleConfirming }) {
+	return (
+		<>
+			<input
+				onClick={(e) => {
+					e.stopPropagation();
+					handleConfirm();
+				}}
+				className={cs.Delete__confirm}
+				type="button"
+				value="DELETE"
+			/>
+			<input
+				onClick={(e) => {
+					e.stopPropagation();
+					toggleConfirming();
+				}}
+				className={cs.Delete__keep}
+				type="button"
+				value="KEEP"
+			/>
+		</>
+	);
+}
+
+export default DeleteButton;
--- client/src/components/HabitDetails/HabitDetails.tsx
+++ client/src/components/HabitDetails/HabitDetails.tsx
@@ -1,146 +1,118 @@
 import dayjs from "dayjs";
-import { useDeleteHabit } from "helpers/api/mutateHabits";
 import { useFetchCompletionsById } from "helpers/api/queryCompletions";
 import { getCompletionSuccessPercentage } from "helpers/completion/completionPercentage";
 import { useClickOutside } from "hooks/useClickOutside";
-import { useToggle } from "hooks/useToggle";
 import { memo, useEffect, useMemo, useRef } from "react";
 import { useRecoilValue } from "recoil";
 import { timescaleAtom } from "state/timescale";
 import { Completion } from "../../../../shared/types/Completion";
 import { Habit } from "../../../../shared/types/Habit";
+import DeleteButton from "./DeleteButton/DeleteButton";
 import cs from "./HabitDetails.module.scss";
 import ProgressIcon from "./ProgressIcon";
 
-function DeleteButton({ habitId }: { habitId: string }) {
-	const { mutate, data } = useDeleteHabit(habitId);
-	const [confirming, toggleConfirming] = useToggle({ initial: false });
+type HabitDetailsProps = {
+	habitData: Habit;
+	completionData: Completion[];
+	toggleDetails: () => void;
+};
 
-	// useEffect(() => {
-	// 	/*  on successful deletion, either
-	//             1. update habitsState
-	//             2. refetch habits
-	//         to ensure the just-deleted habit is removed from view
-	//     */
-	// }, [data]);
+const HabitDetails = memo(({ habitData, toggleDetails }: HabitDetailsProps) => {
+	/* fetch all existing completion entries for this habit. 
+        currently, we only use the completion entries to compute successPercentage,
+        but we might want to do more with the entries later on, so instead of calling an 
+        API endpoint that just returns a percentage, let's keep all the data here for now, 
+        until (if ever) we find that we don't need anything other than successPercentage
+    */
+	const { data, refetch } = useFetchCompletionsById(habitData.habitId);
+	const timescale = useRecoilValue(timescaleAtom);
 
-	function handleConfirm() {
-		mutate(habitId);
+	/* create ref and clickOutside function, and pass to useClickOutside 
+        to turn this component into a proper modal */
+	const modalRef = useRef(null);
+	function handleClickOutside() {
+		toggleDetails();
 	}
+	useClickOutside(modalRef, handleClickOutside, ["Escape"]);
 
-	return !confirming ? (
-		<input
-			className={cs.Delete}
-			type="button"
-			value="Delete habit"
-			onClick={(e) => {
-				e.stopPropagation();
-				toggleConfirming();
-			}}
-		/>
-	) : (
-		<ConfirmDelete {...{ handleConfirm, toggleConfirming }} />
+	useEffect(() => {
+		refetch();
+	}, []);
+
+	const percentage = useMemo(() => {
+		if (!data) return;
+		return getCompletionSuccessPercentage(data, habitData, timescale);
+	}, [data]);
+
+	const trackingSince = dayjs(habitData.startDate || habitData.created).format(
+		"MMMM DD[,] YYYY"
 	);
-}
 
-function ConfirmDelete({ handleConfirm, toggleConfirming }) {
 	return (
-		<>
-			<input
-				onClick={(e) => {
-					e.stopPropagation();
-					handleConfirm();
-				}}
-				className={cs.Delete__confirm}
-				type="button"
-				value="DELETE"
-			/>
-			<input
-				onClick={(e) => {
-					e.stopPropagation();
-					toggleConfirming();
-				}}
-				className={cs.Delete__keep}
-				type="button"
-				value="KEEP"
-			/>
-		</>
-	);
-}
+        /* The wrapper class adds a full-screen slightly transparent background to
+            highlight that the modal is, well, a modal
+        */
+		<div className={cs.Wrapper}>
+			<div ref={modalRef} className={cs.HabitDetails}>
+				<Header>
+					Details for habit{" "}
+					<span className={cs.Name}>{habitData.habitName}</span>
+				</Header>
 
-const HabitDetails = memo(
-	({
-		habitData,
-		completionData,
-		toggleDetails,
-	}: {
-		habitData: Habit;
-		completionData: Completion[];
-		toggleDetails: () => void;
-	}) => {
-		const { data, refetch } = useFetchCompletionsById(habitData.habitId);
-		const timescale = useRecoilValue(timescaleAtom);
+				<ul>
+					{/* TRACKING SINCE */}
+					<Datum label="Age">Tracking this habit since {trackingSince}</Datum>
 
-		const modalRef = useRef(null);
-		function handleClickOutside() {
-			toggleDetails();
-		}
-		useClickOutside(modalRef, handleClickOutside, ["Escape"]);
+                    {/* HABIT COMPLETION SETTINGS */}
+                    <Datum label="Target">
+                        {/* @todo -- PLACEHOLDER */}
+                         You aim to complete this habit X times per Y interval.
+                    </Datum>
 
-		useEffect(() => {
-			refetch();
-		}, []);
-
-		const percentage = useMemo(() => {
-			if (!data) return;
-			return getCompletionSuccessPercentage(data, habitData, timescale);
-		}, [data]);
-
-		const trackingSince = dayjs(habitData.startDate || habitData.created).format(
-			"MMM DD YYYY"
-		);
-
-		return (
-			<div className={cs.Wrapper}>
-				<div ref={modalRef} className={cs.HabitDetails}>
-					<header className={cs.Header}>
-						Details for habit{" "}
-						<span style={{ color: "deepskyblue" }}>{habitData.habitName}</span>
-					</header>
-					<section>
-						<div className={cs.Field}>
-							<span className={cs.Label}>Age</span>
-							Tracking since {trackingSince}
-						</div>
-						{typeof percentage === "number" && (
-							<div className={cs.Field}>
-								<span className={cs.Label}>Progress</span>
-								<div
-									style={{
-										display: "grid",
-										gridTemplateColumns: "max-content 1fr",
-										alignItems: "center",
-										gridGap: "0.5rem",
-									}}
-								>
-									<ProgressIcon size={45} percentage={percentage} /> of{" "}
-									{habitData.completionTimescale}s completed since tracking started
-								</div>
+					{/* PROGRESS */}
+					{!isNaN(percentage) && (
+						<Datum label="Progress">
+							<div
+								style={{
+									display: "grid",
+									gridTemplateColumns: "max-content 1fr",
+									alignItems: "center",
+									gridGap: "0.5rem",
+								}}
+							>
+								<ProgressIcon size={45} percentage={percentage} /> of{" "}
+								{habitData.completionTimescale}s completed since tracking started
 							</div>
-						)}
-						<div className={cs.Field}>
-							<span className={cs.Label}>Current</span> Current streak:
-						</div>
-						<div className={cs.Field}>
-							<span className={cs.Label}>Best</span>
-							Best streak:
-						</div>
-					</section>
-					<DeleteButton habitId={habitData.habitId} />
-				</div>
+						</Datum>
+					)}
+
+					{/* CURRENT STREAK */}
+					<Datum label="Current">Current streak:</Datum>
+
+					{/* BEST STREAK */}
+					<Datum label="Best">Best streak:</Datum>
+				</ul>
+				<DeleteButton habitId={habitData.habitId} />
 			</div>
-		);
-	}
-);
+		</div>
+	);
+});
 
 export default HabitDetails;
+
+function DatumLabel({ children }) {
+	return <span className={cs.Label}>{children}</span>;
+}
+
+function Datum({ children, label }) {
+	return (
+		<li className={cs.Field}>
+			<DatumLabel>{label}</DatumLabel>
+			{children}
+		</li>
+	);
+}
+
+function Header({ children }) {
+	return <header className={cs.Header}>{children}</header>;
+}
