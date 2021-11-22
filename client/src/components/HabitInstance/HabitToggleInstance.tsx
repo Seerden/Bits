b760caccfba68dd7703a54195c4fae1ce25aa400
Christiaan Seerden <christiaanseerden@live.nl>
2021-11-22 21:17:33 +0100

    (HabitToggleInstance) add hover style, title and focus/active higlight

--- client/src/components/HabitInstance/HabitToggleInstance.tsx
+++ client/src/components/HabitInstance/HabitToggleInstance.tsx
@@ -1,12 +1,11 @@
 import { useMutateCompletion } from "helpers/api/mutateCompletion";
 import { useToggle } from "hooks/useToggle";
-import { useCallback, useEffect } from "react";
+import { useCallback } from "react";
 import { RiCheckboxCircleFill, RiCheckboxBlankCircleFill } from "react-icons/ri";
 import { Completion } from "../../../../shared/types/Completion";
-
 import "./HabitToggleInstance.scss";
 
-const HabitToggleInstance = (props: Partial<Completion> & { demo?: boolean }) => {
+const HabitToggleInstance = (props: Partial<Completion>) => {
 	const base = "HabitToggleInstance";
 	const [checked, toggleChecked] = useToggle({ initial: props.completed });
 	const { mutate } = useMutateCompletion();
@@ -23,28 +22,23 @@ const HabitToggleInstance = (props: Partial<Completion> & { demo?: boolean }) =>
 		});
 	}, [checked]);
 
-	const onClick = !props.demo
-		? (e) => {
-				e.preventDefault();
-				toggleChecked();
-				handleClick();
-		  }
-		: (e) => {
-				e.preventDefault();
-				toggleChecked();
-		  };
+	const onClick = (e) => {
+		e.preventDefault();
+		toggleChecked();
+		handleClick();
+	};
 
 	const checkboxProps = {
-		onClick,
 		style: {
 			fill: checked ? "green" : "grey",
 		},
+        className: `${base}__button`
+        
 	};
 	return (
 		<button
-			onClick={(e) => {
-				e.preventDefault();
-			}}
+            title="Click to toggle completion"
+			onClick={onClick}
 			className={`${base}`}
 		>
 			{checked ? (
