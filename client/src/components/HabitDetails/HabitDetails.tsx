ae654b4ec73dd9b67e55ab901c439bd44424a3e7
Christiaan Seerden <christiaanseerden@live.nl>
2021-11-25 17:33:01 +0100

    (chore) switch from tab indentation to spaces

--- client/src/components/HabitDetails/HabitDetails.tsx
+++ client/src/components/HabitDetails/HabitDetails.tsx
@@ -12,118 +12,120 @@ import cs from "./HabitDetails.module.scss";
 import ProgressIcon from "./ProgressIcon";
 
 type HabitDetailsProps = {
-	habitData: Habit;
-	completionData: Completion[];
-	toggleDetails: () => void;
+    habitData: Habit;
+    completionData: Completion[];
+    toggleDetails: () => void;
 };
 
 const HabitDetails = memo(({ habitData, toggleDetails }: HabitDetailsProps) => {
-	/* fetch all existing completion entries for this habit. 
+    /* fetch all existing completion entries for this habit. 
         currently, we only use the completion entries to compute successPercentage,
         but we might want to do more with the entries later on, so instead of calling an 
         API endpoint that just returns a percentage, let's keep all the data here for now, 
         until (if ever) we find that we don't need anything other than successPercentage
     */
-	const { data, refetch } = useFetchCompletionsById(habitData.habitId);
-	const timescale = useRecoilValue(timescaleAtom);
+    const { data, refetch } = useFetchCompletionsById(habitData.habitId);
+    const timescale = useRecoilValue(timescaleAtom);
 
-	/* create ref and clickOutside function, and pass to useClickOutside 
+    /* create ref and clickOutside function, and pass to useClickOutside 
         to turn this component into a proper modal */
-	const modalRef = useRef(null);
-	function handleClickOutside() {
-		toggleDetails();
-	}
-	useClickOutside(modalRef, handleClickOutside, ["Escape"]);
+    const modalRef = useRef(null);
+    function handleClickOutside() {
+        toggleDetails();
+    }
+    useClickOutside(modalRef, handleClickOutside, ["Escape"]);
 
-	useLayoutEffect(() => {
-		refetch();
-	}, []);
+    useLayoutEffect(() => {
+        refetch();
+    }, []);
 
-	const percentage = useMemo(() => {
-		if (!data) return;
-		return getCompletionSuccessPercentage(data, habitData, timescale);
-	}, [data]);
+    const percentage = useMemo(() => {
+        if (!data) return;
+        return getCompletionSuccessPercentage(data, habitData, timescale);
+    }, [data]);
 
-	const trackingSince = dayjs(habitData.startDate || habitData.created).format(
-		"MMMM DD[,] YYYY"
-	);
+    const trackingSince = dayjs(habitData.startDate || habitData.created).format(
+        "MMMM DD[,] YYYY"
+    );
 
-	return (
-		/* The wrapper class adds a full-screen slightly transparent background to
+    return (
+        /* The wrapper class adds a full-screen slightly transparent background to
             highlight that the modal is, well, a modal
         */
-		<div className={cs.Wrapper}>
-			<div ref={modalRef} className={cs.HabitDetails}>
-				<Header>
-					Details for habit <span className={cs.Name}>{habitData.habitName}</span>
-				</Header>
+        <div className={cs.Wrapper}>
+            <div ref={modalRef} className={cs.HabitDetails}>
+                <Header>
+                    Details for habit{" "}
+                    <span className={cs.Name}>{habitData.habitName}</span>
+                </Header>
 
-				<input
-					onClick={() => toggleDetails()}
-					className={cs.Close}
-					type="button"
-					value="Hide details"
-				/>
+                <input
+                    onClick={() => toggleDetails()}
+                    className={cs.Close}
+                    type="button"
+                    value="Hide details"
+                />
 
-				<ul>
-					{/* DESCRIPTION */}
-					{habitData.description.length > 0 && (
-						<Datum label="Description">{habitData.description}</Datum>
-					)}
+                <ul>
+                    {/* DESCRIPTION */}
+                    {habitData.description.length > 0 && (
+                        <Datum label="Description">{habitData.description}</Datum>
+                    )}
 
-					{/* TRACKING SINCE */}
-					<Datum label="Age">Tracking since {trackingSince}</Datum>
+                    {/* TRACKING SINCE */}
+                    <Datum label="Age">Tracking since {trackingSince}</Datum>
 
-					{/* HABIT COMPLETION SETTINGS */}
-					<Datum label="Target">
-						{/* @todo -- PLACEHOLDER */}
-						You aim to complete this habit X times per Y interval.
-					</Datum>
+                    {/* HABIT COMPLETION SETTINGS */}
+                    <Datum label="Target">
+                        {/* @todo -- PLACEHOLDER */}
+                        You aim to complete this habit X times per Y interval.
+                    </Datum>
 
-					{/* PROGRESS */}
-					{!isNaN(percentage) && (
-						<Datum label="Progress">
-							<div
-								style={{
-									display: "grid",
-									gridTemplateColumns: "max-content 1fr",
-									alignItems: "center",
-									gridGap: "0.5rem",
-								}}
-							>
-								<ProgressIcon size={45} percentage={percentage} /> of{" "}
-								{habitData.completionTimescale}s successful since tracking started
-							</div>
-						</Datum>
-					)}
+                    {/* PROGRESS */}
+                    {!isNaN(percentage) && (
+                        <Datum label="Progress">
+                            <div
+                                style={{
+                                    display: "grid",
+                                    gridTemplateColumns: "max-content 1fr",
+                                    alignItems: "center",
+                                    gridGap: "0.5rem",
+                                }}
+                            >
+                                <ProgressIcon size={45} percentage={percentage} /> of{" "}
+                                {habitData.completionTimescale}s successful since tracking
+                                started
+                            </div>
+                        </Datum>
+                    )}
 
-					{/* CURRENT STREAK */}
-					<Datum label="Current">Current streak:</Datum>
+                    {/* CURRENT STREAK */}
+                    <Datum label="Current">Current streak:</Datum>
 
-					{/* BEST STREAK */}
-					<Datum label="Best">Best streak:</Datum>
-				</ul>
-				<DeleteButton toggleDetails={toggleDetails} habitId={habitData.habitId} />
-			</div>
-		</div>
-	);
+                    {/* BEST STREAK */}
+                    <Datum label="Best">Best streak:</Datum>
+                </ul>
+                <DeleteButton toggleDetails={toggleDetails} habitId={habitData.habitId} />
+            </div>
+        </div>
+    );
 });
 
 export default HabitDetails;
 
 function DatumLabel({ children }) {
-	return <span className={cs.Label}>{children}</span>;
+    return <span className={cs.Label}>{children}</span>;
 }
 
 function Datum({ children, label }) {
-	return (
-		<li className={cs.Field}>
-			<DatumLabel>{label}</DatumLabel>
-			{children}
-		</li>
-	);
+    return (
+        <li className={cs.Field}>
+            <DatumLabel>{label}</DatumLabel>
+            {children}
+        </li>
+    );
 }
 
 function Header({ children }) {
-	return <header className={cs.Header}>{children}</header>;
+    return <header className={cs.Header}>{children}</header>;
 }
--- client/src/components/HabitDetails/ProgressIcon.tsx
+++ client/src/components/HabitDetails/ProgressIcon.tsx
@@ -3,37 +3,37 @@ import styled, { Keyframes, keyframes } from "styled-components";
 /// <reference types="styled-components/cssprop" />
 
 const colorByQuarter = {
-	0: "orangered",
-	1: "orange",
-	2: "greenyellow",
-	3: "green",
-	4: "forestgreen",
+    0: "orangered",
+    1: "orange",
+    2: "greenyellow",
+    3: "green",
+    4: "forestgreen",
 };
 
 type Props = {
-	size?: number;
-	percentage: number;
+    size?: number;
+    percentage: number;
 };
 
 // see https://stackoverflow.com/a/53346109/12820947 for circular progress bar
 
 const SVG = styled.svg<{
-	dashes: number;
-	dashOffset: number;
-	buildOffset: Keyframes;
+    dashes: number;
+    dashOffset: number;
+    buildOffset: Keyframes;
 }>`
-	stroke-dasharray: ${(props) => props.dashes};
-	stroke-dashoffset: ${(props) => props.dashOffset};
-	animation: 750ms ${(props) => props.buildOffset} ease-out;
-	transform: rotate(-90deg);
+    stroke-dasharray: ${(props) => props.dashes};
+    stroke-dashoffset: ${(props) => props.dashOffset};
+    animation: 750ms ${(props) => props.buildOffset} ease-out;
+    transform: rotate(-90deg);
 `;
 
 const ProgressIcon = ({ size = 30, percentage }: Props) => {
-	const base = "ProgressIcon";
-	const dashes = 2 * Math.PI * (0.4 * size);
-	const dashOffset = ((100 - percentage) / 100) * dashes;
+    const base = "ProgressIcon";
+    const dashes = 2 * Math.PI * (0.4 * size);
+    const dashOffset = ((100 - percentage) / 100) * dashes;
 
-	const buildOffset = keyframes`
+    const buildOffset = keyframes`
     0% {
         stroke-dashoffset: ${dashes}
     } 100% {
@@ -41,39 +41,39 @@ const ProgressIcon = ({ size = 30, percentage }: Props) => {
     }
     `;
 
-	return (
-		<SVG
-			className={base}
-			height={size}
-			width={size}
-			{...{
-				dashes,
-				dashOffset,
-				buildOffset,
-			}}
-		>
-			<circle
-				className={`${base}__circle`}
-				cx={size / 2}
-				cy={size / 2}
-				r={0.4 * size}
-				stroke={colorByQuarter[String(Math.floor(percentage / 25))]}
-				strokeWidth={4}
-				fill="#333"
-			/>
-			{size >= 40 && (
-				<text
-					className={`${base}__text`}
-					textAnchor="middle"
-					dominantBaseline="middle"
-					x={size / 2}
-					y={size / 2}
-				>
-					{percentage}%
-				</text>
-			)}
-		</SVG>
-	);
+    return (
+        <SVG
+            className={base}
+            height={size}
+            width={size}
+            {...{
+                dashes,
+                dashOffset,
+                buildOffset,
+            }}
+        >
+            <circle
+                className={`${base}__circle`}
+                cx={size / 2}
+                cy={size / 2}
+                r={0.4 * size}
+                stroke={colorByQuarter[String(Math.floor(percentage / 25))]}
+                strokeWidth={4}
+                fill="#333"
+            />
+            {size >= 40 && (
+                <text
+                    className={`${base}__text`}
+                    textAnchor="middle"
+                    dominantBaseline="middle"
+                    x={size / 2}
+                    y={size / 2}
+                >
+                    {percentage}%
+                </text>
+            )}
+        </SVG>
+    );
 };
 
 export default ProgressIcon;
