99abcccc2405009e4c616ea6489331fe73c0e6f2
Christiaan Seerden <christiaanseerden@live.nl>
2021-12-06 20:28:57 +0100

    (chore) optimize withCamelCaseKeys reducer function by assigning property instead of destructuring

--- server/lib/toCamelCase.ts
+++ server/lib/toCamelCase.ts
@@ -1,23 +1,23 @@
 /**
  * Take string `s` that is currently in snake_case and convert it to camelCase
- * 
+ *
  * @usage Convert field names from the database to object properties
  */
 export function snakeToCamelCase(s: string) {
-    const splitString = s.split('_');
+    const splitString = s.split("_");
 
     const n = splitString.length;
 
     if (n == 1) {
-        return s
-    };
+        return s;
+    }
 
     for (let i = 1; i < n; i++) {
-        let substr = splitString[i]
-        splitString[i] = substr[0].toUpperCase() + substr.slice(1)
-    };
+        let substr = splitString[i];
+        splitString[i] = substr[0].toUpperCase() + substr.slice(1);
+    }
 
-    return splitString.join('');
+    return splitString.join("");
 }
 
 /**
@@ -25,23 +25,21 @@ export function snakeToCamelCase(s: string) {
  */
 export function withCamelCaseKeys(rows: Object | Object[]) {
     if (Array.isArray(rows)) {
-        const convertedRows = []
+        const convertedRows = [];
 
-        // @note: we could typecheck each row (recursively) to ensure they're not nested arrays, 
-        //      but with the way our database is set up, we don't have any nested arrays 
+        // @note: we could typecheck each row (recursively) to ensure they're not nested arrays,
+        //      but with the way our database is set up, we don't have any nested arrays
         //      and thus every row should be a single database row, i.e. an object
         for (const row of rows) {
             // reduce object to new object with mapped keys
-            const convertedRow = Object
-                .keys(row)
-                .reduce((acc, property) => ({
-                    ...acc,
-                    [snakeToCamelCase(property)]: row[property]
-                }), {});
+            const convertedRow = Object.keys(row).reduce((acc, property) => {
+                acc[snakeToCamelCase(property)] = row[property];
+                return acc;
+            }, {});
 
-                convertedRows.push(convertedRow);
-        };
+            convertedRows.push(convertedRow);
+        }
 
         return convertedRows;
-    };
+    }
 }
